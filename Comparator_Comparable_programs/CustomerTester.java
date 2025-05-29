7e3beef7cdc03bbec1f0a34c6be36897410a7d08
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-05-29 22:22:30 +0530

    shorting method in Collection
    Program 1:
    ------------
    Write a Program to perform sorting operation on Product class using ArrayList by implementing Comparable interface.
    
    Create a BLC class Product.
    
    Instance variables:
    productNumber-private-Integer
    productName-private-String
    productPrice-private-Double
    
    Create a parameterized constructor to initialize the instance variable of Product class.
    
    Implement Comparable interface to write the logic to sort the Product class based on the Product Name.
    
    Create an ELC class which contains main method, create ArrayList object to hold Product data as well as sort the product data object based on the product name.
    
    Program 2:
    ------------
    Write a Program to perform sorting operation on Customer class using 
    ArrayList by implementing Comparator interface 
    
    Create a BLC class Customer
    Instance variables:
    customerNumber-private-Integer
    customerName-private-String
    customerBill-private-Double
    
    Create a parameterized constructor to initialize the instance variable of Customer class.
    
    Implement Comparator interface to write the logic to sort the Customer class based on the following 
    a) Based on customer number
    b) Based on customer name
    c) Based on customer bill
    Create an ELC class which contains main method, create ArrayList object to hold Customer data as well as sort the Customer data object based on the above criteria

---
+++ Comparator_Comparable_programs/CustomerTester.java
@@ -0,0 +1,56 @@
+package com.Comparator_Comparable_programs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class CustomerTester {
+
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		ArrayList<Customer> cust=new ArrayList<Customer>();
+		
+		Customer p1=new Customer(121, "Bag", 200.10);
+		Customer p2=new Customer(124, "Airpods", 2000.10);
+		Customer p3=new Customer(123, "Laptop", 20000.10);
+		Customer p4=new Customer(120, "Air Bag", 300.10);
+		Customer p5=new Customer(128, "Chopper", 100.10);
+//	Adding into Collection
+		cust.add(p1);
+		cust.add(p2);
+		cust.add(p3);
+		cust.add(p4);
+		cust.add(p5);
+		
+//	Based on Product ID
+		System.out.println("Based on Product ID :");
+		Comparator<Customer> cu=((c1,c2)->{
+			return c1.getProductId()-c2.getProductId();
+		});
+		Collections.sort(cust,cu);
+	cust.forEach((val)->System.out.println(val));
+		
+	
+//	Based on Product Name
+	
+	System.out.println("Based on Product Name");
+	
+	Collections.sort(cust,(Customer c1,Customer c2)->
+		{
+			return c1.getProductName().compareTo(c2.getProductName());
+		}
+	);
+	
+	cust.forEach(System.out::println);
+	
+// Based on price
+	
+	System.out.println("Based on product Price  ");
+	
+	Collections.sort(cust,(c1,c2)->{
+		return (int)(c1.getProductPrice()-c2.getProductPrice());
+	});
+	
+	cust.forEach(System.out::println);
+	}
+}
